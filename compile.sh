616fa8a7c236b7700eba3d261ec9e062553fbcec
Huangzizhou <hzzzkd@mail.ustc.edu.cn>
2020-11-11 01:18:35 -0500

    fix cmake error with mkl on prince

---
+++ compile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Submit job as (build defaults to Release):
+#
+#   sbatch compile.sh
+#   sbatch --export=BUILD='Debug',ALL compile.sh 
+#
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1:00:00
+#SBATCH --mem=8GB
+
+# Load modules
+module purge
+
+module purge
+module load intel/17.0.1
+module load gcc/6.3.0
+module load cmake/gnu/3.11.4
+
+export CC=icc
+export CXX=icpc
+
+export CMAKE_INCLUDE_PATH=$(env | grep _INC= | cut -d= -f2 | xargs | sed -e 's/ /:/g')
+export CMAKE_LIBRARY_PATH=$(env | grep _LIB= | cut -d= -f2 | xargs | sed -e 's/ /:/g')
+
+cmake \
+    -DCMAKE_BUILD_TYPE=release \
+    -DBUILD_SHARED_LIBS::BOOL=ON \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DCMAKE_SKIP_RPATH:BOOL=ON \
+    ..
+
+make

b297a14ee05fa1e7149a5f3457dc810440b54bac
njsyw1997 <44545837+njsyw1997@users.noreply.github.com>
2022-04-15 14:56:52 -0400

    Amgcl Block Solver (#10)
    * Amgcl Block Solver
    
    
    Co-authored-by: Teseo Schneider <teseo@bluewin.ch>

--- src/LinearSolverAMGCL.cpp
+++ src/LinearSolverAMGCL.cpp
@@ -28,42 +28,80 @@ namespace polysolve
             const T *begin_;
             const T *end_;
         };
+
+        json default_params()
+        {
+            json params = R"({
+            "precond": {
+                "relax": {
+                    "type": "spai0"
+                },
+                "class": "amg",
+                "direct_coarse": true,
+                "ncycle": 1,
+                "coarsening": {
+                    "type": "smoothed_aggregation",
+                    "estimate_spectral_radius": false,
+                    "relax": 1.0
+                }
+            },
+            "solver": {
+                "tol": 1e-8,
+                "maxiter": 1000,
+                "type": "cg"
+            }
+        })"_json;
+
+            return params;
+        }
+
+        void set_params(const json &params, json &out)
+        {
+            if (params.contains("max_iter"))
+            {
+                out["solver"]["maxiter"] = params["max_iter"];
+            }
+            if (params.contains("conv_tol"))
+            {
+                out["solver"]["tol"] = params["conv_tol"];
+            }
+            else if (params.contains("tolerance"))
+            {
+                out["solver"]["tol"] = params["tolerance"];
+            }
+            if (params.contains("solver_type"))
+            {
+                out["solver"]["type"] = params["solver_type"];
+            }
+
+            // Patch the stored params with input ones
+            if (params.contains("precond"))
+                out["precond"].merge_patch(params["precond"]);
+            if (params.contains("solver"))
+                out["solver"].merge_patch(params["solver"]);
+
+            if (out["precond"]["class"] == "schur_pressure_correction")
+            {
+                // Initialize the u and p solvers with a tolerance that is comparable to the main solver's
+                if (!out["precond"].contains("usolver"))
+                {
+                    out["precond"]["usolver"] = R"({"solver": {"maxiter": 100}})"_json;
+                    out["precond"]["usolver"]["solver"]["tol"] = 10 * out["solver"]["tol"].get<double>();
+                }
+                if (!out["precond"].contains("usolver"))
+                {
+                    out["precond"]["psolver"] = R"({"solver": {"maxiter": 100}})"_json;
+                    out["precond"]["psolver"]["solver"]["tol"] = 10 * out["solver"]["tol"].get<double>();
+                }
+            }
+        }
     } // namespace
 
     ////////////////////////////////////////////////////////////////////////////////
 
     LinearSolverAMGCL::LinearSolverAMGCL()
     {
-        params_ = R"({
-            "precond": {
-                "relax": {
-                    "degree": 16,
-                    "type": "chebyshev",
-                    "power_iters": 100,
-                    "higher": 2,
-                    "lower": 0.008333333333,
-                    "scale": true
-                },
-                "class": "amg",
-                "max_levels": 6,
-                "direct_coarse": false,
-                "ncycle": 2,
-                "coarsening": {
-                    "type": "smoothed_aggregation",
-                    "estimate_spectral_radius": true,
-                    "relax": 1,
-                    "aggr": {
-                        "eps_strong": 0
-                    }
-                }
-            },
-            "solver": {
-                "tol": 1e-10,
-                "maxiter": 1000,
-                "type": "cg"
-            }
-        })"_json;
-
+        params_ = default_params();
         // NOTE: usolver and psolver parameters are only used if the
         // preconditioner class is "schur_pressure_correction"
         precond_num_ = 0;
@@ -73,43 +111,36 @@ namespace polysolve
     void LinearSolverAMGCL::setParameters(const json &params)
     {
         // Specially named parameters to match other solvers
-        if (params.contains("max_iter"))
+        if (params.contains("block_size"))
         {
-            params_["solver"]["maxiter"] = params["max_iter"];
+            block_size_ = params["block_size"];
         }
-        if (params.contains("conv_tol"))
+        if (block_size_ == 2)
         {
-            params_["solver"]["tol"] = params["conv_tol"];
+            block2_solver_.setParameters(params);
+            return;
         }
-        else if (params.contains("tolerance"))
+        else if (block_size_ == 3)
         {
-            params_["solver"]["tol"] = params["tolerance"];
+            block3_solver_.setParameters(params);
+            return;
         }
 
-        // Patch the stored params with input ones
-        if (params.contains("precond"))
-            params_["precond"].merge_patch(params["precond"]);
-        if (params.contains("solver"))
-            params_["solver"].merge_patch(params["solver"]);
-
-        if (params_["precond"]["class"] == "schur_pressure_correction")
-        {
-            // Initialize the u and p solvers with a tolerance that is comparable to the main solver's
-            if (!params_["precond"].contains("usolver"))
-            {
-                params_["precond"]["usolver"] = R"({"solver": {"maxiter": 100}})"_json;
-                params_["precond"]["usolver"]["solver"]["tol"] = 10 * params_["solver"]["tol"].get<double>();
-            }
-            if (!params_["precond"].contains("usolver"))
-            {
-                params_["precond"]["psolver"] = R"({"solver": {"maxiter": 100}})"_json;
-                params_["precond"]["psolver"]["solver"]["tol"] = 10 * params_["solver"]["tol"].get<double>();
-            }
-        }
+        set_params(params, params_);
     }
 
     void LinearSolverAMGCL::getInfo(json &params) const
     {
+        if (block_size_ == 2)
+        {
+            block2_solver_.getInfo(params);
+            return;
+        }
+        else if (block_size_ == 3)
+        {
+            block3_solver_.getInfo(params);
+            return;
+        }
         params["num_iterations"] = iterations_;
         params["final_res_norm"] = residual_error_;
     }
@@ -117,6 +148,104 @@ namespace polysolve
     ////////////////////////////////////////////////////////////////////////////////
 
     void LinearSolverAMGCL::factorize(const StiffnessMatrix &Ain)
+    {
+        if (block_size_ == 2)
+        {
+            block2_solver_.factorize(Ain);
+            return;
+        }
+        else if (block_size_ == 3)
+        {
+            block3_solver_.factorize(Ain);
+            return;
+        }
+        assert(precond_num_ > 0);
+
+        int numRows = Ain.rows();
+
+        WrappedArray<StiffnessMatrix::StorageIndex> ia(Ain.outerIndexPtr(), numRows + 1);
+        WrappedArray<StiffnessMatrix::StorageIndex> ja(Ain.innerIndexPtr(), Ain.nonZeros());
+        WrappedArray<StiffnessMatrix::Scalar> a(Ain.valuePtr(), Ain.nonZeros());
+        if (params_["precond"]["class"] == "schur_pressure_correction")
+        {
+            std::vector<char> pmask(numRows, 0);
+            for (size_t i = precond_num_; i < numRows; ++i)
+                pmask[i] = 1;
+            params_["precond"]["pmask"] = pmask;
+        }
+
+        // AMGCL takes the parameters as a Boost property_tree (i.e., another JSON data structure)
+        std::stringstream ss_params;
+        ss_params << params_;
+        boost::property_tree::ptree pt_params;
+        boost::property_tree::read_json(ss_params, pt_params);
+        auto A = std::tie(numRows, ia, ja, a);
+        solver_ = std::make_unique<Solver>(A, pt_params);
+        iterations_ = 0;
+        residual_error_ = 0;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    void LinearSolverAMGCL::solve(const Eigen::Ref<const VectorXd> rhs, Eigen::Ref<VectorXd> result)
+    {
+        if (block_size_ == 2)
+        {
+            block2_solver_.solve(rhs, result);
+            return;
+        }
+        else if (block_size_ == 3)
+        {
+            block3_solver_.solve(rhs, result);
+            return;
+        }
+        assert(result.size() == rhs.size());
+        std::vector<double> _rhs(rhs.data(), rhs.data() + rhs.size());
+        std::vector<double> x(result.data(), result.data() + result.size());
+        auto rhs_b = Backend::copy_vector(_rhs, backend_params_);
+        auto x_b = Backend::copy_vector(x, backend_params_);
+
+        assert(solver_ != nullptr);
+        std::tie(iterations_, residual_error_) = (*solver_)(*rhs_b, *x_b);
+
+        std::copy(&(*x_b)[0], &(*x_b)[0] + result.size(), result.data());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    LinearSolverAMGCL::~LinearSolverAMGCL()
+    {
+    }
+
+    template <int BLOCK_SIZE>
+    LinearSolverAMGCL_Block<BLOCK_SIZE>::LinearSolverAMGCL_Block()
+    {
+        params_ = default_params();
+
+        // NOTE: usolver and psolver parameters are only used if the
+        // preconditioner class is "schur_pressure_correction"
+        precond_num_ = 0;
+    }
+
+    template <int BLOCK_SIZE>
+    void LinearSolverAMGCL_Block<BLOCK_SIZE>::setParameters(const json &params)
+    {
+        set_params(params, params_);
+    }
+
+    template <int BLOCK_SIZE>
+    void LinearSolverAMGCL_Block<BLOCK_SIZE>::getInfo(json &params) const
+    {
+        params["num_iterations"] = iterations_;
+        params["final_res_norm"] = residual_error_;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    template <int BLOCK_SIZE>
+    void LinearSolverAMGCL_Block<BLOCK_SIZE>::factorize(const StiffnessMatrix &Ain)
     {
         assert(precond_num_ > 0);
 
@@ -140,8 +269,9 @@ namespace polysolve
         boost::property_tree::ptree pt_params;
         boost::property_tree::read_json(ss_params, pt_params);
 
-        solver_ = std::make_unique<Solver>(std::tie(numRows, ia, ja, a), pt_params);
-
+        auto A = std::tie(numRows, ia, ja, a);
+        auto Ab = amgcl::adapter::block_matrix<dmat_type>(A);
+        solver_ = std::make_unique<Solver>(Ab, pt_params);
         iterations_ = 0;
         residual_error_ = 0;
     }
@@ -150,26 +280,32 @@ namespace polysolve
 
     ////////////////////////////////////////////////////////////////////////////////
 
-    void LinearSolverAMGCL::solve(const Eigen::Ref<const VectorXd> rhs, Eigen::Ref<VectorXd> result)
+    template <int BLOCK_SIZE>
+    void LinearSolverAMGCL_Block<BLOCK_SIZE>::solve(const Eigen::Ref<const VectorXd> rhs, Eigen::Ref<VectorXd> result)
     {
         assert(result.size() == rhs.size());
         std::vector<double> _rhs(rhs.data(), rhs.data() + rhs.size());
         std::vector<double> x(result.data(), result.data() + result.size());
-        auto rhs_b = Backend::copy_vector(_rhs, backend_params_);
-        auto x_b = Backend::copy_vector(x, backend_params_);
+
+        auto rhs_b = amgcl::backend::reinterpret_as_rhs<dmat_type>(_rhs);
+        auto x_b = amgcl::backend::reinterpret_as_rhs<dmat_type>(x);
 
         assert(solver_ != nullptr);
-        std::tie(iterations_, residual_error_) = (*solver_)(*rhs_b, *x_b);
-
-        std::copy(&(*x_b)[0], &(*x_b)[0] + result.size(), result.data());
+        std::tie(iterations_, residual_error_) = (*solver_)(rhs_b, x_b);
+        for (size_t i = 0; i < rhs.size() / BLOCK_SIZE; i++)
+            for (size_t j = 0; j < BLOCK_SIZE; j++)
+            {
+                result[BLOCK_SIZE * i + j] = x_b[i](j);
+            }
     }
 
-    ////////////////////////////////////////////////////////////////////////////////
-
-    LinearSolverAMGCL::~LinearSolverAMGCL()
+    template <int BLOCK_SIZE>
+    LinearSolverAMGCL_Block<BLOCK_SIZE>::~LinearSolverAMGCL_Block()
     {
     }
 
+    template class LinearSolverAMGCL_Block<2>;
+    template class LinearSolverAMGCL_Block<3>;
 } // namespace polysolve
 
 #endif
--- tests/test_solver.cpp
+++ tests/test_solver.cpp
@@ -1,13 +1,44 @@
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 #include <polysolve/FEMSolver.hpp>
-
 #include <catch2/catch.hpp>
 #include <iostream>
 #include <unsupported/Eigen/SparseExtra>
-////////////////////////////////////////////////////////////////////////////////
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <polysolve/LinearSolverAMGCL.hpp>
+//////////////////////////////////////////////////////////////////////////
+
+
 
 using namespace polysolve;
 
+void loadSymmetric(Eigen::SparseMatrix<double> &A, std::string PATH)
+{
+    std::ifstream fin(PATH);
+    long int M, N, L;
+    while (fin.peek() == '%')
+    {
+        fin.ignore(2048, '\n');
+    }
+    fin >> M >> N >> L;
+    A.resize(M, N);
+    A.reserve(L * 2 - M);
+    std::vector<Eigen::Triplet<double>> triple;
+    for (size_t i = 0; i < L; i++)
+    {
+        int m, n;
+        double data;
+        fin >> m >> n >> data;
+        triple.push_back(Eigen::Triplet<double>(m - 1, n - 1, data));
+        if (m != n)
+        {
+            triple.push_back(Eigen::Triplet<double>(n - 1, m - 1, data));
+        }
+    }
+    fin.close();
+    A.setFromTriplets(triple.begin(), triple.end());
+};
 TEST_CASE("all", "[solver]")
 {
     const std::string path = POLYSOLVE_DATA_DIR;
@@ -181,7 +212,7 @@ TEST_CASE("hypre_initial_guess", "[solver]")
 #ifdef POLYSOLVE_WITH_AMGCL
 TEST_CASE("amgcl_initial_guess", "[solver]")
 {
-    const std::string path = POLYSOLVE_DATA_DIR;
+     const std::string path = POLYSOLVE_DATA_DIR;
     Eigen::SparseMatrix<double> A;
     const bool ok = loadMarket(A, path + "/A_2.mat");
     REQUIRE(ok);
@@ -221,27 +252,263 @@ TEST_CASE("amgcl_initial_guess", "[solver]")
 }
 #endif
 
-TEST_CASE("saddle_point_test", "[solver]")
+
+
+ TEST_CASE("saddle_point_test", "[solver]")
+ {
+ #ifdef WIN32
+ #ifndef NDEBUG
+     return;
+ #endif
+ #endif
+     const std::string path = POLYSOLVE_DATA_DIR;
+     Eigen::SparseMatrix<double> A;
+     bool ok = loadMarket(A, path + "/A0.mat");
+     REQUIRE(ok);
+
+     Eigen::VectorXd b;
+     ok = loadMarketVector(b, path + "/b0.mat");
+     REQUIRE(ok);
+
+     auto solver = LinearSolver::create("SaddlePointSolver", "");
+     solver->analyzePattern(A, 9934);
+     solver->factorize(A);
+     Eigen::VectorXd x(A.rows());
+     solver->solve(b, x);
+     const double err = (A * x - b).norm();
+     REQUIRE(err < 1e-8);
+ }
+
+#ifdef POLYSOLVE_WITH_AMGCL
+TEST_CASE("amgcl_blocksolver_small_scale", "[solver]")
 {
-#ifdef WIN32
 #ifndef NDEBUG
     return;
 #endif
-#endif
     const std::string path = POLYSOLVE_DATA_DIR;
     Eigen::SparseMatrix<double> A;
-    bool ok = loadMarket(A, path + "/A0.mat");
+    const bool ok = loadMarket(A, path + "/A_2.mat");
     REQUIRE(ok);
 
-    Eigen::VectorXd b;
-    ok = loadMarketVector(b, path + "/b0.mat");
-    REQUIRE(ok);
-
-    auto solver = LinearSolver::create("SaddlePointSolver", "");
-    solver->analyzePattern(A, 9934);
-    solver->factorize(A);
+    // solver->setParameters(params);
+    Eigen::VectorXd b(A.rows());
+    b.setRandom();
     Eigen::VectorXd x(A.rows());
-    solver->solve(b, x);
-    const double err = (A * x - b).norm();
-    REQUIRE(err < 1e-8);
+    Eigen::VectorXd x_b(A.rows());
+    x.setZero();
+    x_b.setZero();
+    {
+        json solver_info;
+
+        auto solver = LinearSolver::create("AMGCL", "");
+        solver->setParameters(R"({"conv_tol": 1e-8})"_json);
+        solver->analyzePattern(A, A.rows());
+        solver->factorize(A);
+        solver->solve(b, x);
+        solver->getInfo(solver_info);
+
+        REQUIRE(solver_info["num_iterations"] > 0);
+        const double err = (A * x - b).norm();
+        REQUIRE(err < 1e-5);
+    }
+
+    {
+        json solver_info;        
+        auto solver = LinearSolver::create("AMGCL", "");        
+        solver->setParameters(R"({"conv_tol": 1e-8,"block_size": 3})"_json);
+        solver->analyzePattern(A, A.rows());
+        solver->factorize(A);
+        solver->solve(b, x_b);
+        solver->getInfo(solver_info);
+
+        REQUIRE(solver_info["num_iterations"] > 0);
+        const double err = (A * x_b - b).norm();
+        REQUIRE(err < 1e-5);
+    }
 }
+#endif
+
+ #ifdef POLYSOLVE_WITH_AMGCL
+ TEST_CASE("amgcl_blocksolver_b2", "[solver]")
+ {
+#ifndef NDEBUG
+     return;
+#endif
+     const std::string path = POLYSOLVE_DATA_DIR;
+     std::string MatrixName = "gr_30_30.mtx";
+     Eigen::SparseMatrix<double> A;
+     loadSymmetric(A, path + "/" + MatrixName);
+
+     std::cout << "Matrix Load OK" << std::endl;
+    
+     Eigen::VectorXd b(A.rows());
+     b.setRandom();
+     Eigen::VectorXd x(A.rows());
+     Eigen::VectorXd x_b(A.rows());
+     x.setOnes();
+     x_b.setOnes();
+     {
+         amgcl::profiler<> prof("gr_30_30_Scalar");
+         json solver_info;
+         auto solver = LinearSolver::create("AMGCL", "");
+         prof.tic("setup");
+         solver->setParameters(R"({"conv_tol": 1e-8,"max_iter": 1000})"_json);
+         solver->analyzePattern(A, A.rows());
+         solver->factorize(A);
+         prof.toc("setup");
+         prof.tic("solve");
+         solver->solve(b, x);
+         prof.toc("solve");
+         solver->getInfo(solver_info);
+         REQUIRE(solver_info["num_iterations"] > 0);
+         std::cout << solver_info["num_iterations"] << std::endl;
+         std::cout << solver_info["final_res_norm"] << std::endl
+                   << prof << std::endl;
+     }
+     {
+         amgcl::profiler<> prof("gr_30_30_Block");
+         json solver_info;        
+         auto solver = LinearSolver::create("AMGCL", "");
+         prof.tic("setup");
+         solver->setParameters(R"({"conv_tol": 1e-8,"max_iter": 1000,"block_size": 2})"_json);
+         solver->analyzePattern(A, A.rows());
+         solver->factorize(A);
+         prof.toc("setup");
+         prof.tic("solve");
+         solver->solve(b, x_b);
+         prof.toc("solve");
+         solver->getInfo(solver_info);
+         REQUIRE(solver_info["num_iterations"] >0);
+         std::cout<<solver_info["num_iterations"]<<std::endl;
+         std::cout << solver_info["final_res_norm"] << std::endl
+                   << prof << std::endl;
+
+     }
+     REQUIRE((A * x - b).norm() / b.norm() < 1e-7);
+     REQUIRE((A * x_b - b).norm() / b.norm() < 1e-7);
+    
+ }
+ #endif
+
+#ifdef POLYSOLVE_WITH_AMGCL
+TEST_CASE("amgcl_blocksolver_crystm03_CG", "[solver]")
+{
+#ifndef NDEBUG
+    return;
+#endif
+    std::cout << "Polysolve AMGCL Solver" <<std::endl; 
+    const std::string path = POLYSOLVE_DATA_DIR;
+    std::string MatrixName = "crystm03.mtx";
+    Eigen::SparseMatrix<double> A;
+    loadSymmetric(A, path + "/" + MatrixName);
+    std::cout<<"Matrix Load OK"<<std::endl;
+    Eigen::VectorXd b(A.rows());
+    b.setOnes();
+    Eigen::VectorXd x_b(A.rows());
+    x_b.setZero();
+    Eigen::VectorXd x(A.rows());
+    x.setZero();
+    {
+        amgcl::profiler<> prof("crystm03_Block");
+        json solver_info;
+        auto solver = LinearSolver::create("AMGCL", "");
+        prof.tic("setup");
+        solver->setParameters(R"({"conv_tol": 1e-8,"max_iter": 10000,"block_size": 3})"_json);
+        solver->analyzePattern(A, A.rows());
+        solver->factorize(A);
+        prof.toc("setup");
+        prof.tic("solve");
+        solver->solve(b, x_b);
+        prof.toc("solve");
+        solver->getInfo(solver_info);
+        REQUIRE(solver_info["num_iterations"] > 0);
+        std::cout << solver_info["num_iterations"] << std::endl;
+        std::cout << solver_info["final_res_norm"] << std::endl
+                  << prof << std::endl;
+    }
+     {
+         amgcl::profiler<> prof("crystm03_Scalar");
+         json solver_info; 
+         auto solver = LinearSolver::create("AMGCL", "");  
+         prof.tic("setup");
+         solver->setParameters(R"({"conv_tol": 1e-8,"max_iter": 10000})"_json);
+         solver->analyzePattern(A, A.rows());
+         solver->factorize(A);
+         prof.toc("setup");
+         prof.tic("solve");
+         solver->solve(b, x);
+         prof.toc("solve");
+         solver->getInfo(solver_info);
+         REQUIRE(solver_info["num_iterations"] > 0);
+         std::cout<<solver_info["num_iterations"]<<std::endl;
+         std::cout << solver_info["final_res_norm"] << std::endl
+                   << prof << std::endl;
+     }
+     REQUIRE((A * x - b).norm() / b.norm() < 1e-7);
+     REQUIRE((A * x_b - b).norm() / b.norm() < 1e-7);
+}
+#endif
+
+#ifdef POLYSOLVE_WITH_AMGCL
+TEST_CASE("amgcl_blocksolver_crystm03_Bicgstab", "[solver]")
+{
+#ifndef NDEBUG
+    return;
+#endif
+    std::cout << "Polysolve AMGCL Solver" << std::endl;
+    const std::string path = POLYSOLVE_DATA_DIR;
+    std::string MatrixName = "crystm03.mtx";
+    Eigen::SparseMatrix<double> A;
+    loadSymmetric(A, path + "/" + MatrixName);
+
+    std::cout << "Matrix Load OK" << std::endl;
+
+    Eigen::VectorXd b(A.rows());
+    b.setOnes();
+    Eigen::VectorXd x_b(A.rows());
+    x_b.setZero();
+    Eigen::VectorXd x(A.rows());
+    x.setZero();
+    {
+        amgcl::profiler<> prof("crystm03_Block");
+        json solver_info;
+        auto solver = LinearSolver::create("AMGCL", "");
+        prof.tic("setup");
+        solver->setParameters(R"({"conv_tol": 1e-8,"max_iter": 10000,"solver_type": "bicgstab","block_size": 3})"_json);
+        solver->analyzePattern(A, A.rows());
+        solver->factorize(A);
+        prof.toc("setup");
+        prof.tic("solve");
+        solver->solve(b, x_b);
+        prof.toc("solve");
+        solver->getInfo(solver_info);
+        REQUIRE(solver_info["num_iterations"] > 0);
+        std::cout << solver_info["num_iterations"] << std::endl;
+        std::cout << solver_info["final_res_norm"] << std::endl
+                  << prof << std::endl;
+    }
+    {
+        amgcl::profiler<> prof("crystm03_Scalar");
+        json solver_info;
+        auto solver = LinearSolver::create("AMGCL", "");
+        prof.tic("setup");
+        solver->setParameters(R"({"conv_tol": 1e-8,"max_iter": 10000,"solver_type":"bicgstab"})"_json);
+        solver->analyzePattern(A, A.rows());
+        solver->factorize(A);
+        prof.toc("setup");
+        prof.tic("solve");
+        solver->solve(b, x);
+        prof.toc("solve");
+        solver->getInfo(solver_info);
+        REQUIRE(solver_info["num_iterations"] > 0);
+        std::cout << solver_info["num_iterations"] << std::endl;
+        std::cout << solver_info["final_res_norm"] << std::endl
+                  << prof << std::endl;
+    }
+    REQUIRE((A * x - b).norm() / b.norm() < 1e-7);
+    REQUIRE((A * x_b - b).norm() / b.norm() < 1e-7);
+}
+#endif
+
+
+

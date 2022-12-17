8037572b1efbdfe185fe05ba2e99530ac1898923
sdast9 <sdast9@gmail.com>
2022-12-17 16:24:01 -0500

    adds accelerate from dev version of Eigen

--- cmake/recipes/eigen.cmake
+++ cmake/recipes/eigen.cmake
@@ -26,7 +26,6 @@ else()
     FetchContent_Declare(
         eigen
         GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-        GIT_TAG tags/3.3.7
         GIT_SHALLOW TRUE
     )
     FetchContent_GetProperties(eigen)

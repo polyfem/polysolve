d045fc293446be6de0c937d9141f515a5e3d6b3b
Teseo Schneider <teseo@bluewin.ch>
2022-12-20 12:21:17 -0800

    eigen 3.4.0

--- cmake/recipes/eigen.cmake
+++ cmake/recipes/eigen.cmake
@@ -26,7 +26,7 @@ else()
     FetchContent_Declare(
         eigen
         GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-        GIT_TAG tags/3.4
+        GIT_TAG tags/3.4.0
         GIT_SHALLOW TRUE
     )
     FetchContent_GetProperties(eigen)

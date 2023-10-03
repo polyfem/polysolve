d8e17eeddb3f79f1007ac302cc953c1666eeff98
Teseo Schneider <teseo@bluewin.ch>
2023-10-03 14:31:09 -0700

    fixing hessians

--- tests/test_nonlinear_solver.cpp
+++ tests/test_nonlinear_solver.cpp
@@ -3,6 +3,7 @@
 #include <polysolve/nonlinear/Problem.hpp>
 #include <polysolve/nonlinear/Utils.hpp>
 #include <polysolve/Types.hpp>
+#include <polysolve/linear/Solver.hpp>
 
 #include <spdlog/sinks/stdout_color_sinks.h>
 
@@ -44,12 +45,15 @@ public:
     void hessian(const TVector &x, THessian &hessian) override
     {
         hessian.resize(3, 3);
-        sparse_identity(hessian.rows(), hessian.cols());
+        hessian = sparse_identity(hessian.rows(), hessian.cols());
+        hessian *= 2;
     }
+
     void hessian(const TVector &x, Eigen::MatrixXd &hessian) override
     {
         hessian.resize(3, 3);
         hessian.setIdentity();
+        hessian *= 2;
     }
 
     TVector solution() override
@@ -78,11 +82,14 @@ TEST_CASE("non-linear", "[solver]")
     solver_params["first_grad_norm_tol"] = 1e-10;
     solver_params["line_search"]["method"] = "backtracking";
 
+    linear_solver_params["solver"] = "Eigen::SimplicialLDLT";
+    linear_solver_params["precond"] = polysolve::linear::Solver::defaultPrecond();
+
     const double dt = 0.1;
     const double characteristic_length = 1;
 
     static std::shared_ptr<spdlog::logger> logger = spdlog::stdout_color_mt("test_logger");
-
+    logger->set_level(spdlog::level::trace);
     for (auto solver_name : Solver::available_solvers())
     {
         auto solver = Solver::create(solver_name,

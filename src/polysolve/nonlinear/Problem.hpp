a2ad11e0e9e17ded06a227081a22333db4e6e788
Teseo Schneider <teseo@bluewin.ch>
2023-10-17 09:46:10 -0700

    removed stuff from problem

--- src/polysolve/nonlinear/Problem.hpp
+++ src/polysolve/nonlinear/Problem.hpp
@@ -29,7 +29,6 @@ namespace polysolve::nonlinear
         virtual void hessian(const TVector &x, THessian &hessian) = 0;
 
         virtual bool is_step_valid(const TVector &x0, const TVector &x1) const { return true; }
-        virtual bool is_step_collision_free(const TVector &x0, const TVector &x1) const { return true; }
         virtual double max_step_size(const TVector &x0, const TVector &x1) const { return 1; }
 
         virtual void line_search_begin(const TVector &x0, const TVector &x1) {}
@@ -39,13 +38,6 @@ namespace polysolve::nonlinear
         virtual void set_project_to_psd(bool val) {}
 
         virtual void solution_changed(const TVector &new_x) {}
-
-        virtual void init_lagging(const TVector &x) {}
-        virtual void update_lagging(const TVector &x, const int iter_num) {}
-        int max_lagging_iterations() const { return -1; }
-        bool uses_lagging() const { return false; }
-
-        virtual void save_to_file(const TVector &x0) {}
 
         virtual bool stop(const TVector &x) { return false; }
     };

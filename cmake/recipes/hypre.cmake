31b8eed322c00f51643c7f44bf9bf4f3e6b39a40
Ian Su <TzuHsiangSu3000@gmail.com>
2026-04-06 17:14:01 -0400

    workaround hypre uninstall target

--- cmake/recipes/hypre.cmake
+++ cmake/recipes/hypre.cmake
@@ -13,6 +13,16 @@ set(HYPRE_ENABLE_FEI           OFF CACHE INTERNAL "" FORCE)
 set(HYPRE_ENABLE_OPENMP        OFF CACHE INTERNAL "" FORCE)
 set(HYPRE_SHARED               OFF CACHE INTERNAL "" FORCE)
 
+# HYPRE unconditionally defines an "uninstall" target, which conflicts with other buggy libraries
+# as modern cmake requires unique target name. This is a hacky workaround until upstream is fixed.
+macro(add_custom_target _target_name)
+  if("${_target_name}" STREQUAL "uninstall" AND TARGET uninstall)
+    # skip: HYPRE's uninstall target conflicts with an existing one
+  else()
+    _add_custom_target(${_target_name} ${ARGN})
+  endif()
+endmacro()
+
 include(CPM)
 CPMAddPackage(
     NAME hypre

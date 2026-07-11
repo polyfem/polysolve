6e464925383b42095553032252233f56ab8d822a
Julian Panetta <julian.panetta@gmail.com>
2026-07-11 16:59:52 -0700

    Switch to MeshFEMSparse

---
+++ cmake/recipes/meshfemsparse.cmake
@@ -0,0 +1,17 @@
+# MeshFEMSparse
+
+if(TARGET MeshFEMSparse)
+    return()
+endif()
+
+message(STATUS "Third-party: creating target 'MeshFEMSparse'")
+
+set(MESHFEMSPARSE_GIT_REPOSITORY "git@github.com:MeshFEM/MeshFEMSparse.git" CACHE STRING "MeshFEMSparse git repository")
+set(MESHFEMSPARSE_GIT_TAG "main" CACHE STRING "MeshFEMSparse git revision")
+
+include(CPM)
+CPMAddPackage(
+    NAME MeshFEMSparse
+    GIT_REPOSITORY ${MESHFEMSPARSE_GIT_REPOSITORY}
+    GIT_TAG ${MESHFEMSPARSE_GIT_TAG}
+)
